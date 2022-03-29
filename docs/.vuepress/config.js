eb55effedde5d1f0cf1f688c8259ca3997a89418
4923 <www_1216@hufs.ac.kr>
2022-03-29 17:46:47 +0900

    Fix PWA 플러그인 등록 중 오타 수정
    commit dd17ea9f662b7fdbe1b433b754900391aa6f6a70
    plugins: [목록1, 목록2, [목록3, {상세}],] 이와 같은 방식으로 작성해야하는데 대괄호를 중괄호 앞에 붙여버렸다.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,13 +77,13 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    "@vuepress/pwa",
-    [{
-      serviceWorker: true,
-      updatePopup: {
-        message: "새 컨텐츠가 등록되었습니다. 새로고침 버튼을 눌러주세요😀",
-        buttonText: "새로고침",
-      },
-    }],
+    ["@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "새 컨텐츠가 등록되었습니다. 새로고침 버튼을 눌러주세요😀",
+          buttonText: "새로고침",
+        },
+      }],
   ]
 }
