4dd6cef75e7235ddaee45fba80c4f6e3cfd08709
4923 <www_1216@hufs.ac.kr>
2022-04-20 20:16:21 +0900

    Feature utterances label 세팅

--- docs/.vuepress/components/Comment.vue
+++ docs/.vuepress/components/Comment.vue
@@ -14,6 +14,7 @@ export default {
     utterances.src = "https://utteranc.es/client.js";
 
     utterances.setAttribute("issue-term", "pathname"); // pathname|url|title|og:title 중 택 1
+    utterances.setAttribute("label", "Comment💬");
     utterances.setAttribute("theme", "github-light"); // theme 설정 (github-light, github-dark, preferred-color-scheme, github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light) 중 하나 선택
     utterances.setAttribute("repo", `4923/vuepress`); // 주의!! 사용할 repository
 
